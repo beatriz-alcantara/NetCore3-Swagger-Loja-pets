daf90d1220b9852389ea301791e2b18e19dd926c
Beatriz813 <beatrizalcantara813@gmail.com>
2019-11-03 21:33:54 -0300

    First Commit :cry:

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Loja_Pets.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get ()
+        {
+            return Ok("Ok!");
+        }
+    }
+}
